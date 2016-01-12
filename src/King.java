43b4bcec8bed47fc2682c28fa273038eb00670d1
ercetinefe <ercetinefe@gmail.com>
2016-01-13 00:31:17 +0200

    d
    4

--- src/King.java
+++ src/King.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 public class King {
 	public static enum Suit{DIAMOND, CLUB, HEART, SPADE}
 	public static enum Rank{_2, _3, _4, _5, _6, _7, _8, _9, _10, _J, _Q, _K, _A}
+	
 	ArrayList<Card> deck = new ArrayList<Card>();
 	ArrayList<Hand> players = new ArrayList<Hand>();
 	
